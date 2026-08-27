cb5c0bd74af70248227fdd5a735bf10df6cc1a29
PawCare15 <pawcare071@gmail.com>
2026-08-27 14:27:14 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 // 安全托管静态文件
-app.use(express.static(path.join(__dirname, '..')));
+app.use(express.static(__dirname));
 
 function parseUserAgent(userAgent) {
     const parser = new UAParser(userAgent);
